bb0354f947f834446f73de88bd5c3aaa44584544
Carlos G. Oliver <carlos.gonzalez.oliver@gmail.com>
2021-02-23 14:05:28 -0600

    dssr data template

---
+++ prepare_data/dssr_2_graphs.py
@@ -0,0 +1,24 @@
+"""
+
+Build annotated graphs using [x3dna DSSR](http://docs.x3dna.org/dssr-manual.pdf) annotations.
+Requires a x3dna-dssr executable to be in $PATH.
+
+"""
+
+import multiprocessing as mp
+
+from subprocess import run
+
+import networkx as nx
+
+def parse_dssr():
+    pass
+
+def build_one():
+    pass
+
+def build_all():
+    pass
+
+if __name__ == "__main__":
+    pass
