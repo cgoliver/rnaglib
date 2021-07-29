3c1b2ffa0073b4cbff6e7c63f6232b686fb8f67b
Vincentx15 <vincent.mallet96@gmail.com>
2021-07-29 15:15:16 +0200

    cleaned and changed the loader logic again a bit

--- RNAGlib/data_loading/feature_maps.py
+++ RNAGlib/data_loading/feature_maps.py
@@ -5,69 +5,6 @@ Functions to build feature map for each feature type.
 import torch
 
 
-class FeatureMap():
-    def __init__(self, values_list=None, min_val=None, max_val=None):
-        """
-        :param value: feature value we want to encode
-        :param possible_values (list, optional): list of possible values feature
-                                                can take.
-        """
-        self.values_list_raw, self.values_list = (values_list, values_list)
-        self.clean_values()
-
-        self.min_val = min_val
-        self.max_val = max_val
-
-        if not values_list is None:
-            self.dims = len(self.values_list)
-            self.values_enum = {v: k for k, v in enumerate(self.values_list)}
-            self.values_enum_r = dict(enumerate(self.values_list))
-        else:
-            self.dims = 1
-        pass
-
-    def encode(self, value):
-        raise NotImplementedError
-
-    def clean_values(self):
-        pass
-
-
-class NTCode(FeatureMap):
-    """ This feature represents the standard nucleotide type (A,U,C,G,..).
-    We one-hot encode this feature.
-    """
-
-    def clean_values(self):
-        self.values_list = sorted(list(set(map(str.upper, self.values_list))))
-
-    def encode(self, value):
-        """ Assign encoding of `value` according to known possible
-        values.
-        """
-        assert value in self.values_list_raw, f"Not a valid value: {value}, must be {self.values_list}"
-        x = torch.zeros(self.dims)
-        ind = self.values_enum[value.upper()]
-        x[ind] = 1.
-        return x
-
-    def decode(self, one_hot):
-        return self.values_enum_r[torch.where(one_hot)[0].item()]
-
-
-# """
-# TESTING
-# """
-#
-# nt = NTCode(values_list=['A', 'U', 'C', 'G', 'P', 'c', 'a', 'u', 't', 'g'])
-# print(nt.encode('g'))
-# print(nt.encode('G'))
-# assert nt.decode(nt.encode('G')) == 'G'
-
-# import json
-# dict_feat = json.load(open('all_annots.json','r'))
-# ': None, '.join([string[5:] for string in list(dict_feat.keys())])
-
 class OneHotEncoder:
     """
     To one-hot encode this feature.
@@ -84,13 +21,17 @@ class OneHotEncoder:
         """ Assign encoding of `value` according to known possible
         values.
         """
-        x = torch.zeros(self.num_values)
+        x = self.encode_default()
         try:
             ind = self.mapping[value]
             x[ind] = 1.
             return x
         except KeyError:
             return x
+
+    def encode_default(self):
+        x = torch.zeros(self.num_values)
+        return x
 
     def decode(self, one_hot):
         try:
--- RNAGlib/examples/second_example.py
+++ RNAGlib/examples/second_example.py
@@ -14,50 +14,47 @@ from kernels import node_sim
 This script shows a second more complicated example : learn binding protein preferences as well as
 small molecules binding from the nucleotide types and the graph structure
 We also add a pretraining phase based on the R_1 kernel
-
 """
 
-###### Unsupervised phase : ######
+# Choose the data, features and targets to use
+node_features = ['nt_code']
+node_target = ['binding_protein']
 
+###### Unsupervised phase : ######
 # Choose the data and kernel to use for pretraining
+print('Starting to pretrain the network')
 node_sim_func = node_sim.SimFunctionNode(method='R_1', depth=2)
-dataset = loader.GraphDataset(node_simfunc=node_sim_func, node_features='all')
-print(dataset[0][0].to_networkx().edges(data=True))
-data_loader = loader.UnsupervisedLoader(dataset=dataset,
-                                        batch_size=4,
-                                        max_size_kernel=100)
+unsupervised_dataset = loader.UnsupervisedDataset(node_simfunc=node_sim_func, node_features=node_features)
+# print(dataset[0][0].to_networkx().edges(data=True))
+data_loader = loader.Loader(dataset=unsupervised_dataset, max_size_kernel=100)
 train_loader, _, _ = data_loader.get_data()
 
 # Then choose the embedder model and pre_train it
-embedder_model = models.Embedder([10, 10])
+embedder_model = models.Embedder(infeatures_dim=unsupervised_dataset.input_dim,
+                                 dims=[10, 10])
 optimizer = torch.optim.Adam(embedder_model.parameters())
 learn.pretrain_unsupervised(model=embedder_model,
                             optimizer=optimizer,
                             node_sim=node_sim_func,
                             train_loader=train_loader,
-                            num_epochs=1
-                            )
+                            num_epochs=2)
+print()
 print('We have finished pretraining the network, let us fine tune it')
 
 ###### Now the supervised phase : ######
-
-# Choose the data, features and targets to use
-node_features = ['nt_code']
-node_target = ['binding_protein']
-infeatures_dim = len(node_features)
-target_dim = len(node_target)
-
 # GET THE DATA GOING
-loader = loader.SupervisedLoader(node_features=node_features,
-                                 node_target=node_target)
-train_loader, validation_loader, test_loader = loader.get_data()
+supervised_dataset = loader.SupervisedDataset(node_features=node_features,
+                                              node_target=node_target)
+train_loader, validation_loader, test_loader = loader.Loader(dataset=supervised_dataset).get_data()
 
 # Define a model and train it :
 # We first embed our data in 10 dimensions, using the pretrained embedder and then add one classification
-classifier_model = models.Classifier(embedder=embedder_model, last_dim_embedder=10, classif_dims=[target_dim])
+classifier_model = models.Classifier(embedder=embedder_model, last_dim_embedder=10,
+                                     classif_dims=[supervised_dataset.output_dim])
 
 # Finally get the training going
 optimizer = torch.optim.Adam(classifier_model.parameters(), lr=0.001)
 learn.train_supervised(model=classifier_model,
                        optimizer=optimizer,
-                       train_loader=train_loader)
+                       train_loader=train_loader,
+                       num_epochs=2)
