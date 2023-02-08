975c50554d96942273b30cb6e1b1d828edf24437
Vincent <vincent.mallet96@gmail.com>
2023-02-08 11:46:19 +0100

    Added the validation set support for multitask splitting

--- rnaglib/data_loading/graphloader.py
+++ rnaglib/data_loading/graphloader.py
@@ -117,7 +117,7 @@ def full_split(dataset, split_train=0.7, split_valid=0.85):
     return train_set, valid_set, test_set
 
 
-def get_multitask_split(node_targets, graph_index=DEFAULT_INDEX, target_fraction=0.2):
+def get_multitask_split(node_targets, graph_index=DEFAULT_INDEX, fractions=(0.15, 0.15)):
     """
     :param node_targets: A subset of {'binding_protein', 'binding_small-molecule', 'is_modified', 'binding_ion'}
     :param graph_index: should be the opened output of the previous function a dict of dict of dict.
@@ -139,27 +139,41 @@ def get_multitask_split(node_targets, graph_index=DEFAULT_INDEX, target_fraction
                 # Maybe there is something to be made here, but usually it's just absent from the encoding
                 # So summing all values in counter makes sense
                 total_counts[graph_attrs_name] += sum(graph_attrs_counter.values())
+
+    # Then iterate again and stop after having filled all queried sets following the queried fractions
+    splits_to_fill = [set() for _ in range(len(fractions))]
+    filled_splits = []
+    fractions = list(fractions)
+    current_fraction = fractions.pop()
+    currrent_split = splits_to_fill.pop()
+
     query_attrs_insplit = defaultdict(int)
-    # total_nodes_in_split = 0
     copy_query_attrs = node_targets.copy()
-    selected_graphs = set()
-    # Then iterate again and stop after reaching the threshold.
+
     for graph, graph_attrs in graph_index.items():
         for graph_attrs_name, graph_attrs_counter in graph_attrs.items():
             if graph_attrs_name in copy_query_attrs:
                 # Now add this graph and update the splits
-                selected_graphs.add(graph)
+                currrent_split.add(graph)
                 # total_nodes_in_split += len(graph.nodes()) TODO get the number of nodes per graph
                 query_attrs_insplit[graph_attrs_name] += sum(graph_attrs_counter.values())
                 attrs_fraction = float(query_attrs_insplit[graph_attrs_name]) / total_counts[graph_attrs_name]
-                if attrs_fraction > target_fraction:
+                if attrs_fraction > current_fraction:
                     copy_query_attrs.remove(graph_attrs_name)
-        # If we found everything we needed
+        # If we found everything we needed for this split, save it and reset relevant variables
         if len(copy_query_attrs) == 0:
-            break
+            filled_splits = [list(currrent_split)] + filled_splits
+            query_attrs_insplit = defaultdict(int)
+            copy_query_attrs = node_targets.copy()
+            if len(fractions) == 0:
+                break
+            current_fraction = fractions.pop()
+            currrent_split = splits_to_fill.pop()
 
-    train_split, test_split = set(graph_index.keys()) - selected_graphs, selected_graphs
-    return train_split, test_split
+    all_but_train = set().union(*filled_splits)
+    train_split = set(graph_index.keys()) - all_but_train
+    all_splits = [list(train_split)] + filled_splits
+    return all_splits
 
 
 def get_single_task_split(node_target, graph_index=DEFAULT_INDEX, split_train=0.7, split_valid=0.85):
@@ -190,12 +204,15 @@ def meaningful_split_dataset(dataset, split_train=0.7, split_valid=0.85):
 
     # 2nd strategy : for multitask objective, we could also subset, but a random split could end up with one of
     # the categories missing from a set
-    train_split, test_split = get_multitask_split(node_targets=node_targets,
-                                                  target_fraction=split_train)
+    fractions = (1 - split_valid, split_valid - split_train)
+    fractions = (0.01, 0.01)
+    train_split, validation_split, test_split = get_multitask_split(node_targets=node_targets,
+                                                                    fractions=fractions)
 
     train_set = dataset.subset(train_split)
+    validation_set = dataset.subset(validation_split)
     test_set = dataset.subset(test_split)
-    return train_set, None, test_set  # TODO : implement validation using similar strategy
+    return train_set, validation_set, test_set
 
 
 def get_loader(dataset,
@@ -371,7 +388,7 @@ if __name__ == '__main__':
     pass
     node_features = ['nt_code', "alpha", "C5prime_xyz", "is_modified"]
     # node_features = None
-    node_target = ['binding_ion']
+    node_target = ['binding_ion', 'binding_protein']
     # node_target = None
     # node_simfunc = SimFunctionNode(method='R_1', depth=2)
     node_simfunc = None
@@ -386,7 +403,7 @@ if __name__ == '__main__':
         return_type='voxel',
         node_simfunc=node_simfunc)
     train_loader, validation_loader, test_loader = get_loader(dataset=toy_dataset,
-                                                              batch_size=2,
+                                                              batch_size=3,
                                                               num_workers=0)
 
     for i, batch in enumerate(train_loader):
