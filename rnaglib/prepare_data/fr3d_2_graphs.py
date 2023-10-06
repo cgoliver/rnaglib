31c544cc8256dfea1feebda1fd21b53edea6a231
Carlos Oliver <carlos.gonzalez.oliver@gmail.com>
2023-10-06 20:27:16 +0200

    pdbid lower

--- rnaglib/prepare_data/fr3d_2_graphs.py
+++ rnaglib/prepare_data/fr3d_2_graphs.py
@@ -75,6 +75,7 @@ def get_bb(structure, rna_chains, pdbid=''):
 
                 nt_types[f"{pdbid}.{chain.id}.{five_p.id[1]}"] = five_p.get_resname()
                 nt_types[f"{pdbid}.{chain.id}.{three_p.id[1]}"] = three_p.get_resname()
+    print(bb)
     return bb, nt_types
 
 def nt_to_rgl(nt):
@@ -94,7 +95,7 @@ def fr3d_to_graph(rna_path):
         logger.exception(f"Fr3D error {rna_path}")
         return None
 
-    pdbid = rna_path.stem
+    pdbid = rna_path.stem.lower()
     try: 
         rna_chains = get_rna_chains(MMCIF2Dict.MMCIF2Dict(rna_path))
         logger.debug(f"RNA chains in {pdbid}: {rna_chains}")
