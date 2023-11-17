b3d7f17fc4c209309bc7eac9aff667ecf8f4b3e4
Carlos Oliver <carlos.gonzalez.oliver@gmail.com>
2023-11-17 12:00:29 +0100

    Update __init__.py

--- rnaglib/prepare_data/__init__.py
+++ rnaglib/prepare_data/__init__.py
@@ -8,6 +8,7 @@ from .annotations import hariboss_filter
 
 from .dssr_2_graphs import one_rna_from_cif
 from .dssr_2_graphs import cif_to_graph
+from .fr3d_2_graphs import fr3d_to_graph
 
 
 from .chopper import chop_all
@@ -21,7 +22,8 @@ __all__ = ['filter_dot_edges',
            'add_graph_annotations',
            'hariboss_filter',
            'chop_all',
-           'annotate_all'
+           'annotate_all',
+           'fr3d_to_graph'
            ]
 
 classes = __all__
