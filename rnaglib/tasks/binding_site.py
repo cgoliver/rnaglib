d7398d02762c56a15415e9238dceeab5ac89184f
Carlos Oliver <carlos.gonzalez.oliver@gmail.com>
2024-05-07 16:12:14 +0200

    dataset and splits loading and dumping

--- rnaglib/tasks/binding_site.py
+++ rnaglib/tasks/binding_site.py
@@ -5,15 +5,25 @@ from rnaglib.splitters import RandomSplitter
 from rnaglib.data_loading import DEFAULT_INDEX
 
 class BindingSiteDetection(ResidueClassificationTask):
-    def __init__(self, root=None, splitter=None, **kwargs):
+    target_var = "binding_small-molecule"
+    input_var = "nt_code"
+    def __init__(self, root, splitter=None, **kwargs):
         super().__init__(root=root, splitter=splitter, **kwargs)
         pass
     pass
 
     def default_splitter(self):
         return RandomSplitter()
-        pass
 
-    def build_dataset(self, root):
-        dataset = RNADataset()
+    def build_dataset(self):
+        rnas_keep = []
+        for graph, graph_attrs in DEFAULT_INDEX.items():
+            if "node_" + self.target_var in graph_attrs:
+                rnas_keep.append(graph)
+
+        dataset = RNADataset(nt_targets=[self.target_var],
+                             nt_features=[self.input_var],
+                             all_graphs=rnas_keep
+                             )
+
         return dataset
--- rnaglib/tasks/task.py
+++ rnaglib/tasks/task.py
@@ -1,23 +1,50 @@
+import os
 import hashlib
+import json
 from functools import cached_property
 
+from rnaglib.data_loading import RNADataset
+
 class Task:
-    def __init__(self, root=None, splitter=None):
+    def __init__(self, root, splitter=None):
         self.root = root
         if splitter is None:
             self.splitter = self.default_splitter()
         else:
             self.splitter = splitter
 
-
         self.dataset = self._build_dataset(root)
         self.split()
 
+        if not os.path.exists(root):
+            os.mkdir(root)
+            self.write()
+        pass
+
+    def write(self):
+        with open(os.path.join(self.root, 'dataset.txt'), 'w') as ds:
+            [ds.write("\n".join([rna['rna_name'] for rna in self.dataset.to_list()]))]
+        with open(os.path.join(self.root, 'train_idx.txt'), 'w') as idx:
+            [idx.write(str(ind) + "\n") for ind in self.train_ind]
+        with open(os.path.join(self.root, 'val_idx.txt'), 'w') as idx:
+            [idx.write(str(ind) + "\n") for ind in self.val_ind]
+        with open(os.path.join(self.root, 'test_idx.txt'), 'w') as idx:
+            [idx.write(str(ind) + "\n") for ind in self.test_ind]
+        with open(os.path.join(self.root, "task_id.txt"), "w") as tid:
+                  tid.write(self.task_id)
         pass
 
     def split(self):
         """ Sets train, val, and test indices"""
-        train_ind, val_ind, test_ind = self.splitter(self.dataset)
+        if not os.path.exists(os.path.join(self.root, "train_idx.txt")):
+            print(">>> Computing splits...")
+            train_ind, val_ind, test_ind = self.splitter(self.dataset)
+        else:
+            print(">>> Loading splits...")
+            train_ind = [int(ind) for ind in open(os.path.join(self.root, "train_idx.txt"), 'r').readlines()]
+            val_ind = [int(ind) for ind in open(os.path.join(self.root, "val_idx.txt"), 'r').readlines()]
+            test_ind = [int(ind) for ind in open(os.path.join(self.root, "test_idx.txt"), 'r').readlines()]
+
         self.train_ind = train_ind
         self.val_ind = val_ind
         self.test_ind = test_ind
@@ -25,16 +52,21 @@ class Task:
     
     def _build_dataset(self, root):
         # check if dataset exists and load
-
-        # else call self.build_dataset()
-        return self.build_dataset(root)
+        if os.path.exists(os.path.join(self.root, "dataset.txt")):
+            print(">>> Loading dataset")
+            with open(os.path.join(root, "dataset.txt"), 'r') as ds:
+                all_graphs = [g.strip() for g in ds.readlines()]
+            return RNADataset(all_graphs=all_graphs)
+        else:
+            print(">>> Building dataset")
+            return self.build_dataset()
 
     def build_dataset(self, root):
         raise NotImplementedError
 
 
     @cached_property
-    def task_hash(self):
+    def task_id(self):
         """ Task hash is a hash of all RNA ids and node IDs in the dataset"""
         h = hashlib.new('sha256')
         for rna in self.dataset.to_list():
@@ -46,7 +78,7 @@ class Task:
         return h.hexdigest()
 
     def __eq__(self, other):
-        return self.task_hash() == other.task_hash()
+        return self.task_id == other.task_id
 
 class ResidueClassificationTask(Task):
     def __init__(self, **kwargs):
