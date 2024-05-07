c42394d6a42373c6fabb06a60a7007fd7fc11635
Carlos Oliver <carlos.gonzalez.oliver@gmail.com>
2024-05-07 15:00:33 +0200

    splitter implemented

--- rnaglib/tasks/task.py
+++ rnaglib/tasks/task.py
@@ -2,14 +2,26 @@ import hashlib
 from functools import cached_property
 
 class Task:
-    def __init__(self, root=None, splitter= None):
+    def __init__(self, root=None, splitter=None):
         self.root = root
-        self.splitter = splitter
+        if splitter is None:
+            self.splitter = self.default_splitter()
+        else:
+            self.splitter = splitter
+
+
         self.dataset = self._build_dataset(root)
+        self.split()
+
         pass
 
     def split(self):
-        raise NotImplementedError
+        """ Sets train, val, and test indices"""
+        train_ind, val_ind, test_ind = self.splitter(self.dataset)
+        self.train_ind = train_ind
+        self.val_ind = val_ind
+        self.test_ind = test_ind
+        pass
     
     def _build_dataset(self, root):
         # check if dataset exists and load
@@ -23,11 +35,14 @@ class Task:
 
     @cached_property
     def task_hash(self):
+        """ Task hash is a hash of all RNA ids and node IDs in the dataset"""
         h = hashlib.new('sha256')
-        rna_ids = ""
         for rna in self.dataset.to_list():
-            rna_ids += rna['rna_name']
-        h.update(rna_ids.encode("utf-8"))
+            h.update(rna['rna_name'].encode("utf-8"))
+            for nt in rna['rna'].nodes():
+                h.update(nt.encode("utf-8"))
+        [h.update(str(i).encode("utf-8")) for i in self.train_ind]
+        [h.update(str(i).encode("utf-8")) for i in self.val_ind]
         return h.hexdigest()
 
     def __eq__(self, other):
