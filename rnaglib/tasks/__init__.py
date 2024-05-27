7e37eeb21c8154193def4129884d0b811ec31f5c
luiswyss <47637876+luiswyss@users.noreply.github.com>
2024-05-27 15:08:17 +0200

    new task for protein binding site prediction

--- rnaglib/tasks/__init__.py
+++ rnaglib/tasks/__init__.py
@@ -1,12 +1,13 @@
 from .task import Task
 from .task import ResidueClassificationTask
-from .binding_site import BindingSiteDetection
+from .binding_site import BindingSiteDetection, ProteinBindingSiteDetection
 from .benchmark_binding_site import BenchmarkLigandBindingSiteDetection
 
 __all__ = [
            'Task',
            'ResidueClassificationTask',
            'BindingSiteDetection',
+           'ProteinBindingSiteDetection',
            'BenchmarkLigandBindingSiteDetection'
            ]
 
--- rnaglib/tasks/binding_site.py
+++ rnaglib/tasks/binding_site.py
@@ -3,6 +3,7 @@ from rnaglib.tasks import ResidueClassificationTask
 from rnaglib.splitters import RandomSplitter
 
 from rnaglib.utils import load_index
+import requests
 
 class BindingSiteDetection(ResidueClassificationTask):
     target_var = "binding_small-molecule"
@@ -28,3 +29,58 @@ class BindingSiteDetection(ResidueClassificationTask):
                              )
 
         return dataset
+
+class ProteinBindingSiteDetection(ResidueClassificationTask):
+    target_var = "binding_protein"
+    input_var = "nt_code"
+
+    def __init__(self, root, splitter=None, **kwargs):
+        self.ribosomal_rnas = self.get_ribosomal_rnas()
+        super().__init__(root=root, splitter=splitter, **kwargs)
+        pass
+    pass
+
+    def default_splitter(self):
+        return RandomSplitter()
+
+    def build_dataset(self):
+        graph_index = load_index()
+        rnas_keep = []
+        for graph, graph_attrs in graph_index.items():
+            rna_id = graph.split(".")[0]
+            if "node_" + self.target_var in graph_attrs and rna_id not in self.ribosomal_rnas:
+                rnas_keep.append(rna_id) 
+
+        dataset = RNADataset(nt_targets=[self.target_var],
+                             nt_features=[self.input_var],
+                             rna_filter=lambda x: x.graph['pdbid'][0].lower() in rnas_keep
+                             )
+        return dataset
+
+
+    def get_ribosomal_rnas(self):
+        url = "https://search.rcsb.org/rcsbsearch/v2/query"
+        query = {
+            "query": {
+                "type": "terminal",
+                "service": "text",
+                "parameters": {
+                    "attribute": "struct_keywords.pdbx_keywords",
+                    "operator": "contains_phrase",
+                    "value": "ribosome"
+                }
+            },
+            "return_type": "entry",
+            "request_options": {
+                "return_all_hits": True
+            }
+        }
+        response = requests.post(url, json=query)
+        if response.status_code == 200:
+            data = response.json()
+            ribosomal_rnas = [result['identifier'] for result in data['result_set']]
+            return ribosomal_rnas
+        else:
+            print(f"Failed to retrieve data: {response.status_code}")
+            print(response.text)
+            return []
