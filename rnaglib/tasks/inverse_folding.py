f853c668bd7d810fe4a944fc5cf5cf2e7396b217
luiswyss <47637876+luiswyss@users.noreply.github.com>
2024-05-31 15:52:35 +0200

    improved folding task

--- rnaglib/tasks/inverse_folding.py
+++ rnaglib/tasks/inverse_folding.py
@@ -3,13 +3,13 @@ from rnaglib.tasks import RNAClassificationTask
 from rnaglib.splitters import RandomSplitter
 
 from rnaglib.utils import load_index
-import requests
-
+import pandas as pd
+import ast
 
 
 class gRNAde(RNAClassificationTask):
-    target_var = "sequence" #needs to be replaced with an actual var
-    input_var = "coordinates" #needs to be replaced with an actual var
+    target_var = "nt_code" #in rna graph
+    input_var = "index" #this is wrong and should be the graph. needs rework of task superclass
 
     def __init__(self, root, splitter=None, **kwargs):
         super().__init__(root=root, splitter=splitter, **kwargs)
@@ -25,25 +25,40 @@ class gRNAde(RNAClassificationTask):
     def evaluate(self, data, predictions):
         sequence_recovery_rates = []
         for pred, true in zip(predictions , data.y):
-            result = sequence_recovery(true, pred)
+            result = self.sequence_recovery(true, pred)
             sequence_recovery_rates.append(result)
         average_srr = sum(sequence_recovery_rates) / len(sequence_recovery_rates)
         
+        # at one point, evaluate on these rnas:['1CSL', '1ET4', '1F27', '1L2X', '1LNT', '1Q9A', '1U8D', '1X9C', '1XPE', '2GCS', '2GDI', '2OEU', '2R8S', '354D'] to compare to gRNAde
+
+
         return average_srr
     
     def default_splitter(self):
         return RandomSplitter()
+        # SingleStateSplit
+        # MultiStateSplit
 
     def build_dataset(self, root):
-        graph_index = load_index()
+        #load metadata from gRNAde if it fails, print link
+        try:
+            metadata = pd.read_csv('data/gRNAde_metadata.csv')
+        except FileNotFoundError:
+            print('Download the metadata from https://drive.google.com/file/d/1lbdiE1LfWPReo5VnZy0zblvhVl5QhaF4/ and place it in the ./data dir')
+
+        # generate list
         rnas_keep = []
-        for graph, graph_attrs in graph_index.items():
-            if "node_" + self.target_var in graph_attrs:
-                rnas_keep.append(graph.split(".")[0])
+
+        for sample in metadata['id_list']:
+            per_sample_list = ast.literal_eval(sample)
+            rnas_keep.extend(per_sample_list)
+        #remove extra info from strings
+        rnas_keep = [x.split('_')[0] for x in rnas_keep]
 
         dataset = RNADataset(nt_targets=[self.target_var],
                              nt_features=[self.input_var],
-                             rna_filter=lambda x: x.graph['pdbid'][0].lower() in rnas_keep
+                             redundancy='all',
+                             rna_filter=lambda x: x.graph['pdbid'][0] in rnas_keep
                              )
 
         return dataset
