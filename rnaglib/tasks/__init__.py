9095ffeafb2696e67c8304aedbcf742b6cdd3361
luiswyss <47637876+luiswyss@users.noreply.github.com>
2024-06-05 21:42:25 +0200

    submission experiment

--- rnaglib/tasks/__init__.py
+++ rnaglib/tasks/__init__.py
@@ -1,6 +1,6 @@
 from .task import Task
 from .task import ResidueClassificationTask, RNAClassificationTask
-from .benchmark_binding_site import BenchmarkLigandBindingSiteDetection
+from .benchmark_binding_site import BenchmarkLigandBindingSiteDetection,BenchmarkLigandBindingSiteDetectionEmbeddings, BenchmarkProteinBindingSiteDetection, BenchmarkChemicalModification, BenchmarkProteinBindingSiteDetectionEmbeddings, BenchmarkChemicalModificationEmbeddings 
 from .binding_site import BindingSiteDetection, ProteinBindingSiteDetection, BindingDetection, ProteinBindingDetection, ChemicalModification
 from .inverse_folding import InverseFolding, gRNAde
 
@@ -11,6 +11,11 @@ __all__ = [
            'BindingSiteDetection',
            'ProteinBindingSiteDetection',
            'BenchmarkLigandBindingSiteDetection',
+           'BenchmarkLigandBindingSiteDetectionEmbeddings',
+           'BenchmarkProteinBindingSiteDetection', 
+           'BenchmarkChemicalModification', 
+           'BenchmarkProteinBindingSiteDetectionEmbeddings', 
+           'BenchmarkChemicalModificationEmbeddings',
            'BindingDetection',
            'ProteinBindingDetection',
            'ChemicalModification',
