47c94e0f7ad39f584bc5126dd6fc2280effd068e
Carlos <carlos@ozeki.io>
2024-09-03 14:41:32 -0400

    feat computer tests

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -5,9 +5,7 @@ from rnaglib.tasks import RNAFamilyTask
 
 class TaskTest(unittest.TestCase):
 
-    """
     def test_RNAFamilyTask(self):
         with tempfile.TemporaryDirectory() as tmp:
             ta = RNAFamilyTask(root=tmp, debug=True)
         pass
-    """
