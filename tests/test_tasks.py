7f717c3b981ac8b9d2fa299cd7f1a13a806a49d5
Carlos <carlos@ozeki.io>
2024-09-03 14:59:42 -0400

    remove task test

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -3,9 +3,11 @@ import tempfile
 
 from rnaglib.tasks import RNAFamilyTask
 
+"""
 class TaskTest(unittest.TestCase):
 
     def test_RNAFamilyTask(self):
         with tempfile.TemporaryDirectory() as tmp:
             ta = RNAFamilyTask(root=tmp, debug=True)
         pass
+"""
