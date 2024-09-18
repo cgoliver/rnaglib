b5704795ebfe1a6d966600a15285950e3ba079fe
vincent <vincent.mallet96@gmail.com>
2024-09-18 15:43:43 +0200

    several tiny modifications

--- src/rnaglib/transforms/__init__.py
+++ src/rnaglib/transforms/__init__.py
@@ -1,7 +1,7 @@
 from .transform import *
 
-from rnaglib.transforms.partition import *
-from rnaglib.transforms.filter import *
 from rnaglib.transforms.annotate import *
 from rnaglib.transforms.featurize import *
+from rnaglib.transforms.filter import *
+from rnaglib.transforms.partition import *
 from rnaglib.transforms.represent import *
