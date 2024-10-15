79bc058055955ed10c0906bdde8eec9d5d4f1f3b
wisskarrou <wisskarrou@gmail.com>
2024-10-15 18:18:31 +0200

    refactor set_loaders, get_split_datasets and get_split_loaders and add set_datasets method

--- src/rnaglib/tasks/task.py
+++ src/rnaglib/tasks/task.py
@@ -66,8 +66,6 @@ class Task:
         if save:
             self.write()
 
-        # self.set_loaders()
-
     def process(self) -> RNADataset:
         """Tasks must implement this method. Executing the method should result in a list of ``.json`` files
         saved in ``{root}/dataset``. All the RNA graphs should contain all the annotations needed to run the task (e.g. node/edge attributes)
@@ -91,42 +89,46 @@ class Task:
         """Calls the splitter and returns train, val, test splits."""
         return self.splitter(dataset)
 
-    def set_loaders(self):
-        """Sets the dataset and loader properties.
+    def set_datasets(self):
+        """Sets the train, val and test datasets
         Call this each time you modify ``self.dataset``."""
 
-        self.train_dataset, self.val_dataset, self.test_dataset = (
-            self.get_split_datasets()
-        )
+        self.train_dataset = self.dataset.subset(self.train_ind)
+        self.val_dataset = self.dataset.subset(self.val_ind)
+        self.test_dataset = self.dataset.subset(self.test_ind)
 
-        self.train_dataloader, self.val_dataloader, self.test_dataloader = (
-            self.get_split_loaders()
-        )
+    def set_loaders(self, **dataloader_kwargs):
+        """Sets the dataloader properties.
+        Call this each time you modify ``self.dataset``."""
 
-    def get_split_datasets(self):
-        train_set = self.dataset.subset(self.train_ind)
-        val_set = self.dataset.subset(self.val_ind)
-        test_set = self.dataset.subset(self.test_ind)
-        return train_set, val_set, test_set
-
-    def get_split_loaders(self, **dataloader_kwargs):
-        # If datasets were not already precomputed
-        if "train_dataset" not in self.__dict__:
-            train_set, val_set, test_set = self.get_split_datasets()
+        self.set_datasets()
 
         # If no collater is provided we need one
         if dataloader_kwargs is None:
-            dataloader_kwargs = {"collate_fn": Collater(train_set)}
+            dataloader_kwargs = {"collate_fn": Collater(self.train_dataset)}
         if "collate_fn" not in dataloader_kwargs:
-            collater = Collater(train_set)
+            collater = Collater(self.train_dataset)
             dataloader_kwargs["collate_fn"] = collater
 
         # Now build the loaders
-        train_loader = DataLoader(dataset=train_set, **dataloader_kwargs)
+        self.train_dataloader = DataLoader(dataset=self.train_dataset, **dataloader_kwargs)
         dataloader_kwargs["shuffle"] = False
-        val_loader = DataLoader(dataset=val_set, **dataloader_kwargs)
-        test_loader = DataLoader(dataset=test_set, **dataloader_kwargs)
-        return train_loader, val_loader, test_loader
+        self.val_dataloader = DataLoader(dataset=self.val_dataset, **dataloader_kwargs)
+        self.test_dataloader = DataLoader(dataset=self.test_dataset, **dataloader_kwargs)
+
+    def get_split_datasets(self, recompute=True):
+        # If datasets were not already computed or if we want to recompute them to account
+        # for changes in the global dataset
+        if recompute or "train_dataset" not in self.__dict__:
+            self.set_datasets()
+        return self.train_dataset, self.val_dataset, self.test_dataset
+
+    def get_split_loaders(self, recompute=True, **dataloader_kwargs):
+        # If dataloaders were not already precomputed or if we want to recompute them to account
+        # for changes in the global dataset
+        if recompute or "train_dataloader" not in self.__dict__:
+            self.set_loaders(**dataloader_kwargs)
+        return self.train_dataloader, self.val_dataloader, self.test_dataloader
 
     def evaluate(self, model) -> dict:
         raise NotImplementedError
