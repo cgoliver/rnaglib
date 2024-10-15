324b92996eb16b030e73bd73a00f660488f1da84
luiswyss <47637876+luiswyss@users.noreply.github.com>
2024-10-15 14:53:13 +0200

    bug fixes, imports, splitter draft

--- src/rnaglib/splitters/__init__.py
+++ src/rnaglib/splitters/__init__.py
@@ -1,6 +1,7 @@
 from .splitting_utils import random_split
 
 from .splitters import Splitter, RandomSplitter, NameSplitter
+from .splitters import TR60TR18Splitter
 from .splitters import default_splitter_tr60_tr18, get_ribosomal_rnas
 from .splitters import SPLITTING_VARS
 from .splitting_utils import split_dataset
@@ -9,6 +10,7 @@ from .similarity_splitter import ClusterSplitter, RNAalignSplitter, CDHitSplitte
 __all__ = ['Splitter',
            'RandomSplitter',
            'NameSplitter',
+           'TR60TR18Splitter',
            'ClusterSplitter',
            'RNAalignSplitter',
            'CDHitSplitter',
