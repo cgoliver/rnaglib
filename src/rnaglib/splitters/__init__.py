c30856c454dace483f3f8b83544d69204e4ef1d1
luiswyss <47637876+luiswyss@users.noreply.github.com>
2024-10-28 09:53:30 +0100

    split size issue remains but splitter code runs now

--- src/rnaglib/splitters/__init__.py
+++ src/rnaglib/splitters/__init__.py
@@ -1,24 +1,23 @@
 from .splitting_utils import random_split
 
 from .splitters import Splitter, RandomSplitter, NameSplitter
-from .splitters import TR60TR18Splitter
 from .splitters import default_splitter_tr60_tr18, get_ribosomal_rnas
 from .splitters import SPLITTING_VARS
 from .splitting_utils import split_dataset
 from .similarity_splitter import ClusterSplitter, RNAalignSplitter, CDHitSplitter
 
-__all__ = ['Splitter',
-           'RandomSplitter',
-           'NameSplitter',
-           'TR60TR18Splitter',
-           'ClusterSplitter',
-           'RNAalignSplitter',
-           'CDHitSplitter',
-           'default_splitter_tr60_tr18',
-           'get_ribosomal_rnas',
-           'SPLITTING_VARS',
-           'random_split',
-           'split_dataset'
-           ]
+__all__ = [
+    "Splitter",
+    "RandomSplitter",
+    "NameSplitter",
+    "ClusterSplitter",
+    "RNAalignSplitter",
+    "CDHitSplitter",
+    "default_splitter_tr60_tr18",
+    "get_ribosomal_rnas",
+    "SPLITTING_VARS",
+    "random_split",
+    "split_dataset",
+]
 
 classes = __all__
