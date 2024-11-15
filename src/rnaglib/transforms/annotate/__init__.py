c1114a28c3d785c85d99615573348988f88c86c2
wisskarrou <wisskarrou@gmail.com>
2024-11-15 12:14:51 +0100

    Wrote new version of RNA_ligand task and implemented its associated annotator and partition

--- src/rnaglib/transforms/annotate/__init__.py
+++ src/rnaglib/transforms/annotate/__init__.py
@@ -2,4 +2,5 @@ from .names import *
 from .rnafm import RNAFMTransform
 from .rfam import *
 from .binding_sites import BindingSiteAnnotator
+from .RNA_ligand import LigandAnnotator
 from .dummy import *
