6cc3f27a00811b285c28b60da45accc1fc743b08
luiswyss <47637876+luiswyss@users.noreply.github.com>
2025-01-07 13:26:54 +0100

    balanced splitter

--- src/rnaglib/splitters/__init__.py
+++ src/rnaglib/splitters/__init__.py
@@ -1,9 +1,9 @@
-from .splitting_utils import random_split
+"""imports for splitting module"""
 
+from .splitting_utils import split_dataset, random_split
 from .splitters import Splitter, RandomSplitter, NameSplitter
 from .splitters import default_splitter_tr60_tr18, get_ribosomal_rnas
 from .splitters import SPLITTING_VARS
-from .splitting_utils import split_dataset
 from .similarity_splitter import ClusterSplitter, RNAalignSplitter, CDHitSplitter
 
 __all__ = [
