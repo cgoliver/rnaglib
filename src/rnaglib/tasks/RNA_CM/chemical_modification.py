51ace541ec91b1d3b0827b0d6531f9b74a6051be
Vincentx15 <vincent.mallet96@gmail.com>
2025-01-09 10:45:51 +0100

    Small factoring of the process() in_memory switch

--- src/rnaglib/tasks/RNA_CM/chemical_modification.py
+++ src/rnaglib/tasks/RNA_CM/chemical_modification.py
@@ -6,7 +6,6 @@ from rnaglib.transforms import FeaturesComputer
 from rnaglib.transforms import ResidueAttributeFilter, ComposeFilters
 from rnaglib.transforms import DummyFilter
 from rnaglib.transforms import PDBIDNameTransform
-from rnaglib.utils import dump_json
 
 
 class ChemicalModification(ResidueClassificationTask):
@@ -39,10 +38,6 @@ class ChemicalModification(ResidueClassificationTask):
         for rna in dataset:
             if rna_filter.forward(rna):
                 rna = add_name(rna)["rna"]
-                if self.in_memory:
-                    all_rnas.append(rna)
-                else:
-                    all_rnas.append(rna.name)
-                    dump_json(os.path.join(self.dataset_path, f"{rna.name}.json"), rna)
+                self.add_rna_to_building_list(all_rnas=all_rnas,rna=rna)
         dataset = self.create_dataset_from_list(all_rnas)
         return dataset
