00d135d6c03c79cb20af4941856e853d0742ea8a
cgoliver <carlos.gonzalez.oliver@gmail.com>
2025-01-09 17:00:18 -0500

    patch database build

--- src/rnaglib/transforms/annotate/ss.py
+++ src/rnaglib/transforms/annotate/ss.py
@@ -26,18 +26,22 @@ class SecondaryStructureTransform(Transform):
 
     def forward(self, rna_dict):
         pdbid = rna_dict["rna"].graph["pdbid"]
-        out = forgi.load_rna(str(Path(self.structures_dir) / f"{pdbid}.cif"))
         ss_dict = {}
         seq_dict = {}
-        for cg in out:
-            (
-                pdb,
-                seq,
-                ss,
-            ) = cg.to_fasta_string().split()
-            chain_id = pdb.split("_")[-1].strip()
-            ss_dict[chain_id] = ss
-            seq_dict[chain_id] = seq
+        try:
+            out = forgi.load_rna(str(Path(self.structures_dir) / f"{pdbid}.cif"))
+        except:
+            pass
+        else:
+            for cg in out:
+                (
+                    pdb,
+                    seq,
+                    ss,
+                ) = cg.to_fasta_string().split()
+                chain_id = pdb.split("_")[-1].strip()
+                ss_dict[chain_id] = ss
+                seq_dict[chain_id] = seq
         rna_dict["rna"].graph["ss"] = ss_dict
         rna_dict["rna"].graph["seq"] = seq_dict
         return rna_dict
