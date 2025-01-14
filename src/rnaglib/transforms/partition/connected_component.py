2f6b058dfa7e538163f389b81902ce42208fdf2a
wisskarrou <wisskarrou@gmail.com>
2025-01-14 15:18:21 +0100

    update connected_component.py

--- src/rnaglib/transforms/partition/connected_component.py
+++ src/rnaglib/transforms/partition/connected_component.py
@@ -4,7 +4,7 @@ import networkx as nx
 from rnaglib.transforms import Transform, PartitionTransform
 
 
-class ConnectedComponentSplitTransform(PartitionTransform):
+class ConnectedComponentPartition(PartitionTransform):
     """Split up an RNA by connected components. Yields all nodes belonging to the same connected 
     component at a time.
     """
@@ -12,5 +12,10 @@ class ConnectedComponentSplitTransform(PartitionTransform):
     def forward(self, rna_dict: dict) -> Iterator[dict]:
         g = rna_dict["rna"]
         connected_components = nx.weakly_connected_components(g)
-        for c in connected_components:
-            yield {"rna": g.subgraph(c).copy()}
+        for i, c in enumerate(connected_components):
+            connected_component_subgraph = g.subgraph(c).copy()
+            try:
+                connected_component_subgraph.name = g.name + "_" + str(i)
+            except:
+                connected_component_subgraph.name = g.graph["pdbid"].lower() + "_" + str(i)
+            yield {"rna": connected_component_subgraph}
