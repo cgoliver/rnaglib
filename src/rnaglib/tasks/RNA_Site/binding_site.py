08aa90219ae2e6caf1b9a99774324e7b5cddeebc
wisskarrou <wisskarrou@gmail.com>
2025-01-14 15:10:15 +0100

    update RNA_Site

--- src/rnaglib/tasks/RNA_Site/binding_site.py
+++ src/rnaglib/tasks/RNA_Site/binding_site.py
@@ -7,6 +7,7 @@ from rnaglib.data_loading.create_dataset import (
     nt_filter_split_chains,
 )  # check whether needed after rna-fm integration
 import os
+import numpy as np
 
 from rnaglib.data_loading import RNADataset
 from rnaglib.transforms import FeaturesComputer
@@ -17,6 +18,7 @@ from rnaglib.transforms import ResidueAttributeFilter
 from rnaglib.transforms import PDBIDNameTransform, ChainNameTransform
 from rnaglib.transforms import BindingSiteAnnotator
 from rnaglib.transforms import ChainFilter, ComposeFilters
+from rnaglib.transforms import ConnectedComponentPartition
 
 
 class BenchmarkBindingSite(ResidueClassificationTask):
@@ -36,11 +38,12 @@ class BenchmarkBindingSite(ResidueClassificationTask):
     def process(self) -> RNADataset:
         # Define your transforms
         chain_filter = ChainFilter(SPLITTING_VARS["PDB_TO_CHAIN_TR60_TE18"])
-        self.filters_list.append(chain_filter)
+        filters_list = [chain_filter]
+        rna_filter = ComposeFilters(filters_list)
         
-        rna_filter = ComposeFilters(self.filters_list)
         bs_annotator = BindingSiteAnnotator()
         namer = ChainNameTransform()
+        connected_components_partition = ConnectedComponentPartition()
 
         dataset = RNADataset(
             debug=self.debug,
@@ -49,12 +52,18 @@ class BenchmarkBindingSite(ResidueClassificationTask):
             rna_id_subset=SPLITTING_VARS["PDB_TO_CHAIN_TR60_TE18"].keys(),
         )
 
+        
+
         # Run through database, applying our filters
         all_rnas = []
         os.makedirs(self.dataset_path, exist_ok=True)
         for rna in dataset:
             if rna_filter.forward(rna):
-                rna = bs_annotator(rna)
+                for rna_connected_component in connected_components_partition(rna):
+                    if self.size_thresholds is not None:
+                        if not self.size_filter(rna_connected_component):
+                            pass
+                rna = bs_annotator(rna_connected_component)
                 rna = namer(rna)['rna']
                 self.add_rna_to_building_list(all_rnas=all_rnas, rna=rna)
         dataset = self.create_dataset_from_list(all_rnas)
@@ -77,11 +86,14 @@ class BindingSite(ResidueClassificationTask):
 
     def process(self) -> RNADataset:
         # Define your transforms
-        attribute_filter = ResidueAttributeFilter(attribute=self.target_var, value_checker=lambda val: val is not None)
-        self.filters_list.append(attribute_filter)
+        rna_filter = ResidueAttributeFilter(attribute=self.target_var, value_checker=lambda val: val is not None)
+        connected_components_partition = ConnectedComponentPartition()
 
-        rna_filter = ComposeFilters(self.filters_list)
-        add_name = PDBIDNameTransform()
+        protein_content_filter = ResidueAttributeFilter(attribute="protein_content_8.0", aggregation_mode="aggfunc", value_checker=lambda x: x < 10, aggfunc=np.mean)
+        connected_component_filters_list = [protein_content_filter]
+        if self.size_thresholds is not None:
+            connected_component_filters_list.append(self.size_filter)
+        connected_component_filters = ComposeFilters(connected_component_filters_list)
 
         # Run through database, applying our filters
         dataset = RNADataset(debug=self.debug, in_memory=self.in_memory)
@@ -89,8 +101,11 @@ class BindingSite(ResidueClassificationTask):
         os.makedirs(self.dataset_path, exist_ok=True)
         for rna in dataset:
             if rna_filter.forward(rna):
-                rna = add_name(rna)["rna"]
-                self.add_rna_to_building_list(all_rnas=all_rnas, rna=rna)
+                for rna_connected_component in connected_components_partition(rna):
+                    if not connected_component_filters.forward(rna_connected_component):
+                        continue
+                    rna = rna_connected_component["rna"]
+                    self.add_rna_to_building_list(all_rnas=all_rnas, rna=rna)
         dataset = self.create_dataset_from_list(all_rnas)
         return dataset
 
