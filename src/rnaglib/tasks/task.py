ed93d14a1c183870e19ec40543ce1a787743f0f8
cgoliver <carlos.gonzalez.oliver@gmail.com>
2025-02-21 14:35:16 -0600

    fix task dump

--- src/rnaglib/tasks/task.py
+++ src/rnaglib/tasks/task.py
@@ -75,8 +75,10 @@ class Task:
             print("no split found, splitting")
             self.split(self.dataset)
 
-        if self.save and not os.path.exists(self.root):
+        if self.save:
             self.write()
+            with open(Path(self.root) / "done.txt", "w") as f:
+                f.write("")
 
         # compute metadata
         self.describe()
