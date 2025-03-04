d050f4fafb6c57c872db595833fa9ed70119fa4c
cgoliver <carlos.gonzalez.oliver@gmail.com>
2025-03-04 07:08:11 -0600

    allow device selection in pyg model

--- src/rnaglib/learning/task_models.py
+++ src/rnaglib/learning/task_models.py
@@ -19,7 +19,7 @@ class PygModel(torch.nn.Module):
         dropout_rate=0.5,
         multi_label=False,
         final_activation="sigmoid",
-	device=None
+        device=None
     ):
         super().__init__()
         self.num_node_features = num_node_features
@@ -77,12 +77,15 @@ class PygModel(torch.nn.Module):
                 self.final_activation = torch.nn.Identity()
 
         self.optimizer = None
-        if torch.cuda.is_available():
-            self.device = "gpu"
-        elif torch.backends.mps.is_available():
-            self.device = "mps"
+        if device is None:
+            if torch.cuda.is_available():
+                self.device = "gpu"
+            elif torch.backends.mps.is_available():
+                self.device = "mps"
+            else:
+                self.devide = "cpu"
         else:
-            self.devide = "cpu"
+            self.device = device
 
     def forward(self, data):
         x, edge_index, edge_type, batch = data.x, data.edge_index, data.edge_attr, data.batch
