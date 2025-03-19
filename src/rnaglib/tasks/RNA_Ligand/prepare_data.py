a421f8b0df41fd81d1ff0e3b149948192a3b2920
wisskarrou <wisskarrou@gmail.com>
2025-03-19 18:49:57 +0100

    change prepare_data of RNA_Ligand to make it return JSONs and not CSV anymore

--- src/rnaglib/tasks/RNA_Ligand/prepare_data.py
+++ src/rnaglib/tasks/RNA_Ligand/prepare_data.py
@@ -1,5 +1,7 @@
 import numpy as np
+from collections import defaultdict
 import pandas as pd
+import networkx as nx
 import json
 from tqdm import tqdm
 from pathlib import Path
@@ -22,51 +24,36 @@ binding_pocket_filters = ComposeFilters(binding_pocket_filters_list)
 
 
 # Run through database, applying our filters
-binding_pockets = pd.DataFrame(columns = ['bp_id','nid','ligand','ligand_smiles','RNA'])
-bp_ids_list = []
-nids_list = []
-ligands_list = []
-ligand_smiles_list = []
-rnas_list = []
-bp_dict = {}
+bp_dict = defaultdict(list)
 ligands_dict = {}
 bp_id = 0
 dataset_size = len(dataset)
-for i, rna in tqdm(enumerate(dataset)):
+for i, rna in tqdm(enumerate(dataset),total=dataset_size):
     cif = str(Path(dataset.structures_path) / f"{rna['rna'].graph['pdbid'].lower()}.cif")
     mmcif_dict = MMCIF2Dict(cif)
-    for ligand in rna['rna'].graph['ligands']:
-        if not mmcif_dict['_chem_comp.type'][mmcif_dict['_chem_comp.id'].index(ligand['name'])] in ['RNA linking','DNA linking']:
-            existing_nodes = set(rna['rna'].nodes())
-            clean_rna_neighs = [nei for nei in ligand['rna_neighs'] if nei in existing_nodes]
-            binding_pocket_nodes = bfs(rna['rna'], clean_rna_neighs, label="LW")
+    lig_to_nodes = defaultdict(list)
+    lig_info = nx.get_node_attributes(rna['rna'], 'binding_small-molecule-6.0A')
+    for node, ligand in lig_info.items():
+        if not ligand is None:
+            lig_to_nodes[tuple(ligand['id'])].append(node)
+            ligands_dict[node] = ligand['name']
+    for ligand_id, binding_pocket in lig_to_nodes.items():
+        ligand_name = ligand_id[0][2:]
+        if not mmcif_dict['_chem_comp.type'][mmcif_dict['_chem_comp.id'].index(ligand_name)] in ['RNA linking','DNA linking']:
+            binding_pocket_nodes = bfs(rna['rna'], binding_pocket, label="LW")
             n_hop = 2
             while len(binding_pocket_nodes)<40 and n_hop<4:
                 binding_pocket_nodes = bfs(rna['rna'], binding_pocket_nodes, label="LW")
                 n_hop += 1
             binding_pocket = rna['rna'].subgraph(binding_pocket_nodes).copy()
             if protein_content_filter.forward({'rna':binding_pocket}):
-                try:
-                    bp_dict[rna['rna'].name].append(binding_pocket_nodes)
-                except:
-                    bp_dict[rna['rna'].name] = [binding_pocket_nodes]
-                bp_id += 1
+                bp_dict[rna['rna'].name].append(list(binding_pocket_nodes))
+                bp_id += 1    
 
-                for rna_neighbor in tqdm(binding_pocket_nodes):
-                    bp_ids_list.append(bp_id)
-                    nids_list.append(rna_neighbor)
-                    ligands_list.append(ligand['name'])
-                    ligand_smiles_list.append(ligand['smiles'])
-                    rnas_list.append(rna['rna'].name)
+bp_dict = {key:list(bp_dict[key]) for key in bp_dict}
 
+with open('bp_dict_lastversion.json', 'w') as bp_dict_json:
+    json.dump(bp_dict, bp_dict_json)
 
-bp_dict2 = {key:list(bp_dict[key]) for key in bp_dict}
-with open('bp_dict3.json', 'w') as f:
-    json.dump(bp_dict2, f)
-
-binding_pockets['bp_id'] = pd.Series(bp_ids_list)
-binding_pockets['nid'] = pd.Series(nids_list)
-binding_pockets['ligand'] = pd.Series(ligands_list)
-binding_pockets['ligand_smiles'] = pd.Series(ligand_smiles_list)
-binding_pockets['RNA'] = pd.Series(rnas_list)
-binding_pockets.to_csv('binding_pockets4.csv')
+with open('ligands_dict_lastversion.json', 'w') as ligands_dict_json:
+    json.dump(ligands_dict, ligands_dict_json)
