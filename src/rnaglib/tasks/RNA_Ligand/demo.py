c039d1597254c4cd00719a8dd36a9fad7c0ef26f
wisskarrou <wisskarrou@gmail.com>
2026-07-21 19:28:10 +0200

    adapt RNA-ligand demo to the end of the task-specific balanced sampler

--- src/rnaglib/tasks/RNA_Ligand/demo.py
+++ src/rnaglib/tasks/RNA_Ligand/demo.py
@@ -5,7 +5,7 @@ from rnaglib.transforms import GraphRepresentation
 from rnaglib.learning.task_models import PygModel
 
 # Creating task 
-ta = LigandIdentification('RNA_Ligand', recompute=True, use_balanced_sampler=True)
+ta = LigandIdentification('RNA_Ligand', recompute=True)
 
 # Adding representation
 ta.add_representation(GraphRepresentation(framework="pyg"))
