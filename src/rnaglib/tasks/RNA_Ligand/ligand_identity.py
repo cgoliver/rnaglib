48daf32231186cc6af2bb5fcdb085fad65a11810
Vincentx15 <vincent.mallet96@gmail.com>
2026-08-03 18:57:39 +0200

    fix download() content-length crash and LigandIdentification None-family bug
    download() raised TypeError instead of a retryable error when a failed
    request lacked a content-length header, masking a plain 404 as 5 retries
    of an unrelated crash. LigandIdentification let pockets with an unmapped
    ligand family through in debug mode, crashing sorted() on a mixed str/None
    set. Also skip the two RNA_CM task tests pending the v3.0.0 Zenodo release,
    and log open items in TODO.md.

--- src/rnaglib/tasks/RNA_Ligand/ligand_identity.py
+++ src/rnaglib/tasks/RNA_Ligand/ligand_identity.py
@@ -1,3 +1,4 @@
+
 import os
 import json
 import collections
@@ -98,7 +99,7 @@ class LigandIdentification(RNAClassificationTask):
                         continue
                     ligand_code = collections.Counter(codes).most_common(1)[0][0]
                     family = self.family_map.get(ligand_code)
-                    if family in self.admissible_families or self.debug:
+                    if family is not None and (family in self.admissible_families or self.debug):
                         pocket.graph[self.target_var] = family
                         self.add_rna_to_building_list(all_rnas=all_binding_pockets, rna=pocket)
         dataset = self.create_dataset_from_list(all_binding_pockets)
